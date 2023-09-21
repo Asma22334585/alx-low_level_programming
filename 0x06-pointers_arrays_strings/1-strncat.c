ecf1139cf3fada821503080897c8287cdd8ca6ec
Asma22334585 <guesmihadil0@gmail.com>
2023-09-21 11:35:05 -0700

    my commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,9 +18,12 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		a++;
 	}
-	for (b = 0; b < n && src[b] != '\0' ; b++)
+	b = 0;
+	while (b < n && src[b] != '\0')
 	{
-	dest[a] = src[b];
+		dest[a] = src[b];
+		a++;
+		b++;
 	}
 	dest[a] = '\0';
 	return (dest);

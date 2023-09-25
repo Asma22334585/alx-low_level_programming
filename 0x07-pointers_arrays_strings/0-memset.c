9b166d7853295159ffa1981a09f88f93e575bad2
Asma22334585 <guesmihadil0@gmail.com>
2023-09-25 12:55:11 -0700

    my commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @n: number of bytes
+ * @b: constant byte
+ * @s: the memory area
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; n > 0; x++; n--)
+	{
+		s[x] = b;
+	}
+	return (s);
+}

e1671529dffaa02ed6169b64df08fbfae522c48b
Asma22334585 <guesmihadil0@gmail.com>
2023-10-11 13:06:52 -0700

    my commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,23 +10,29 @@
  */
 int main(int argc, char *argv[])
 {
-	int b;
-	char *p =( char *)main;
+	int b, i;
+	char *c;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
 	b = atoi(argv[1]);
-
 	if (b < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	while (b--)
-		printf("%02hhx%s ", *p++, b ? " " : "\n");
+	c = (char *)main;
+	for (i = 0; i < b; i++)
+	{
+		if (i == b - 1)
+		{
+			printf("%02hhx\n", c[i]);
+			break;
+		}
+		printf("%02hhx ", c[i]);
+	}
 	return (0);
 }
